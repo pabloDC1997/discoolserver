a862f4f4e67d5d238e9830bcaa010f42a33f17ef
Pablo Couto <akpablo1997@gmail.com>
2018-11-15 21:11:17 -0200

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ module.exports = function() {
         host: 'localhost',
         port: '3306',
         user: 'root',
-        password: 'uchiha13',
+        password: '-----', //FIXME
         database: 'discool_db'
     });
 }
